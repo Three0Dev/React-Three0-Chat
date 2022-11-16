fb45e91978c0fd23204bd3d0e2e44dd59872e7b8
Sree Grandhe <sgrandhe@utexas.edu>
2022-11-16 16:07:32 -0600

    Empty credentials

--- src/env.js
+++ src/env.js
@@ -1,10 +1,10 @@
 export const env = {
     // NOTE: Replace with your own project configuration
     three0Config: {
-        "contractName": "project-three0-v4srk4xxhd178ha6emfycw.testnet",
-        "projectId": "project-three0-v4srk4xxhd178ha6emfycw",
-        "chainType": "NEAR_TESTNET"
+        "contractName": "",
+        "projectId": "",
+        "chainType": ""
     },
     // NOTE: Add your own docstore database here
-    chatAppDBURL: "/orbitdb/zdpuB187H1AkQvsk6ryaGw1fAC6wFmscc9LmqfQmATmr92CxY/newdb"
+    chatAppDBURL: ""
 }
